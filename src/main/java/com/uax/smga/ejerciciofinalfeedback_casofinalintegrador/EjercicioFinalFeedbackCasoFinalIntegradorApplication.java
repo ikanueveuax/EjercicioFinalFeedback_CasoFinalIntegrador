b156c252ba5bde9b8958fa9de37cdcfa53cd437c
ikanueveuax <ikarakul@myuax.com>
2025-01-28 23:22:45 +0100

    Initial commit

---
+++ src/main/java/com/uax/smga/ejerciciofinalfeedback_casofinalintegrador/EjercicioFinalFeedbackCasoFinalIntegradorApplication.java
@@ -0,0 +1,13 @@
+package com.uax.smga.ejerciciofinalfeedback_casofinalintegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioFinalFeedbackCasoFinalIntegradorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjercicioFinalFeedbackCasoFinalIntegradorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uax/smga/ejerciciofinalfeedback_casofinalintegrador/EjercicioFinalFeedbackCasoFinalIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uax.smga.ejerciciofinalfeedback_casofinalintegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioFinalFeedbackCasoFinalIntegradorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
